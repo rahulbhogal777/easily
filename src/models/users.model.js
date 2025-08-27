83dd5ea1b6ce5af35e3b3e18a9005f14d4bfdd6a
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-08-27 16:00:13 +0530

    correct model mistake

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,9 +1,9 @@
 class User {
     constructor(id, name, email, password) {
-        this.id = id,
-        this.name = name,
-        this.email = email,
-        this.password = password
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     static addUser(name, email, password) {
