32a9311768e8b4d1df954424a7e2160c26b8da47
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-09-03 14:19:45 +0530

    add job list page

--- src/models/jobs.model.js
+++ src/models/jobs.model.js
@@ -7,7 +7,7 @@ class JobModel {
     companyname,
     salary,
     applyby,
-    skillrequired,
+    skills_required,
     numberofopenings,
     jobposted,
     applicants
@@ -19,19 +19,57 @@ class JobModel {
     this.companyname = companyname;
     this.salary = salary;
     this.applyby = applyby;
-    this.skillrequired = skillrequired;
+    this.skills_required = skills_required;
     this.numberofopenings = numberofopenings;
     this.jobposted = jobposted;
     this.applicants = applicants;
   }
-    
-    static getJobs() {
-        return jobs;
-    }
+
+  static getJobs() {
+    return jobs;
+  }
 }
 
 var jobs = [
-    
+  new JobModel(
+    1,
+    "IT",
+    "SDE",
+    "Gurgaon HR IND Remote",
+    "Coding Ninjas",
+    "6-10 LPA",
+    "Apply By 30 Aug 2023",
+    ["React", "NodeJS", "JS", "SQL", "MongoDB", "Express", "AWS"],
+    10,
+    "23 july 2023",
+    5
+  ),
+  new JobModel(
+    2,
+    "IT",
+    "SDE",
+    "Pune IND On-Site",
+    "GoDigit",
+    "6-10 LPA",
+    "Apply By 30 Aug 2023",
+    ["Angular", "JS", "SQL", "MongoDB", "Express", "AWS"],
+    10,
+    "23 july 2023",
+    5
+  ),
+  new JobModel(
+    3,
+    "IT",
+    "SDE",
+    "Banglore IND",
+    "Juspay",
+    "20-26 LPA",
+    "Apply By 30 Aug 2023",
+    ["React", "NodeJS", "JS", "SQL", "MongoDB", "Express", "AWS"],
+    10,
+    "23 july 2023",
+    5
+  ),
 ];
 
 export default JobModel;
