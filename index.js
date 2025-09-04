1bb5b15baf09edca4f35719db59bf361e691842e
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-09-04 09:54:10 +0530

    add public folder

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
 app.use(ejsLayout);
+app.set("view engine", "ejs")
 app.set("views", path.join(path.resolve(), "src", "views"));
 app.set(
   "layout",
