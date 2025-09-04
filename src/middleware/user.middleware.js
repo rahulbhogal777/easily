be6b5285d0f2fe1ca236df1748dcfacff20148a6
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-09-04 14:21:20 +0530

    modify the cookie value

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,8 +1,8 @@
 export const user = (req, res, next) => {
   res.locals.user = req.session.user || null;
 
-  res.cookie("user", new Date().toISOString(), {
+  res.cookie("user", JSON.stringify(res.locals.user), {
     maxAge: 2 * 24 * 60 * 60 * 1000,
   });
-  next();
+    next();
 };
