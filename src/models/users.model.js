80cd5fd6ca0f79fa384d0e09f7b8b92108d37629
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-09-04 12:30:48 +0530

    some type modification

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,21 +1,22 @@
 class User {
-    constructor(id, name, email, password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
+  constructor(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
 
-    static addUser(name, email, password) {
-        const id = users.length + 1;
-        const user = new User(id, name, email, password);
-        users.push(user);
-    }
+  static addUser(name, email, password) {
+    const id = users.length + 1;
+    const user = new User(id, name, email, password);
+    users.push(user);
+  }
 
-    static findUser(email, password) {
-        return users.find((user) => user.email === email && user.password === password);
-    }
-
+  static findUser(email, password) {
+    return users.find(
+      (user) => user.email === email && user.password === password
+    );
+  }
 }
 
 let users = [];
