8c86fe47aa352e8cf56fe0de6fd3a4c00cadfda7
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-09-08 12:33:23 +0530

    add logos

--- src/models/jobs.model.js
+++ src/models/jobs.model.js
@@ -5,24 +5,30 @@ class JobModel {
     job_designation,
     job_location,
     company_name,
+    company_founded,
     salary,
     apply_by,
     skills_required,
     numberofopenings,
+    experience,
     jobposted,
-    applicants
+    applicants,
+    logo,
   ) {
     this.id = id;
     this.job_category = jobcategory;
     this.job_designation = job_designation;
     this.job_location = job_location;
     this.company_name = company_name;
+    this.company_founded = company_founded;
     this.salary = salary;
     this.apply_by = apply_by;
     this.skills_required = skills_required;
     this.number_of_openings = numberofopenings;
+    this.experience = experience;
     this.job_posted = jobposted;
     this.applicants = applicants;
+    this.logo = logo;
   }
 
   static getDataById(id) {
@@ -57,13 +63,16 @@ var jobs = [
     "IT",
     "SDE",
     "Gurgaon HR IND Remote",
-    "CodingNinjas",
+    "Coding Ninjas",
+    2016,
     "6-10 LPA",
     "Apply By 30 Aug 2023",
     ["React", "NodeJS", "JS", "SQL", "MongoDB", "Express", "AWS"],
     10,
+    "1-2 yrs",
     "23 july 2023",
-    []
+    [],
+    "https://s3.amazonaws.com/beamstart/2019/Nov/25/d6921b8f8bf3ec4f1005c1f9afa08798.jpeg"
   ),
   new JobModel(
     2,
@@ -71,12 +80,15 @@ var jobs = [
     "SDE",
     "Pune IND On-Site",
     "GoDigit",
+    2012,
     "6-10 LPA",
     "Apply By 30 Aug 2023",
     ["Angular", "JS", "SQL", "MongoDB", "Express", "AWS"],
     10,
+    "2-3 yrs",
     "23 july 2023",
-    []
+    [],
+    "https://play-lh.googleusercontent.com/E48vP0Wlf9lEPfOGwDR8CTupJw7fHuwVjlZiOke_GZITViUdUHXxn1A1jZI-qjcjMw"
   ),
   new JobModel(
     3,
@@ -84,12 +96,15 @@ var jobs = [
     "SDE",
     "Banglore IND",
     "Juspay",
+    2011,
     "20-26 LPA",
     "Apply By 30 Aug 2023",
     ["React", "NodeJS", "JS", "SQL", "MongoDB", "Express", "AWS"],
     10,
+    "3-5 yrs",
     "23 july 2023",
-    []
+    [],
+    "https://www.merchantpaymentsecosystem.com/img/companies/juspay.jpg"
   ),
 ];
 
