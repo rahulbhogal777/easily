530c359b5d4e1985fa53e3c7ce14112acf03864d
rahulbhogal777 <rahulbhogal777@gmail.com>
2025-09-10 11:42:56 +0530

    do some formating

--- src/middleware/mail.middleware.js
+++ src/middleware/mail.middleware.js
@@ -1,31 +1,31 @@
 import nodemailer from "nodemailer";
 import fs from "fs";
 
-const template = fs.readFileSync("public/html/mailTemplate.html", { encoding: "utf-8" });
+const template = fs.readFileSync("public/html/mailTemplate.html", {
+  encoding: "utf-8",
+});
 
 export const mailMiddleware = (req, res, next) => {
-
-    try {
-        const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "rahulbhogal77@gmail.com",
-            pass: "dsqj cprc awgj zajm",
-        }
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "rahulbhogal77@gmail.com",
+        pass: "dsqj cprc awgj zajm",
+      },
     });
 
     const mailOptions = {
-        from: "rahulbhogal77@gmail.com",
-        to: req.body.email,
-        subject: 'Welcome to Our Service',
-        html: template
-        }
-        const info = transporter.sendMail(mailOptions);
-        console.log("Email sent: " + req.body.email);
-        next();
-    } catch (error) {
-        console.log("Error sending email: ", error);
-        res.render("404");
-    }
-}
-   
+      from: "rahulbhogal77@gmail.com",
+      to: req.body.email,
+      subject: "Welcome to Our Service",
+      html: template,
+    };
+    const info = transporter.sendMail(mailOptions);
+    console.log("Email sent: " + req.body.email);
+    next();
+  } catch (error) {
+    console.log("Error sending email: ", error);
+    res.render("404");
+  }
+};
